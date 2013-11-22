5e84252460722e4b926b0dd19ffb10b621102815
Tyler Thompson <tyler.thompson@erfmarketing.com>
2013-11-22 11:10:40 -0500

    Experimenting with package.js and removed FastClick from foundation.js

--- package.js
+++ package.js
@@ -6,7 +6,11 @@ Package.on_use(function (api) {
 	api.use('jquery', 'client');
 
 	var path = Npm.require('path');
-	api.add_files(path.join('client', 'custom.modernizr.js'), 'client');
-	api.add_files(path.join('client', 'foundation.min.js'), 'client');
+	api.add_files(path.join('client', 'vendor/custom.modernizr.js'), 'client');
+	api.add_files(path.join('client', 'vendor/fastclick.js'), 'client');
+	api.add_files(path.join('client', 'vendor/jquery.autocomplete.js'), 'client');
+	api.add_files(path.join('client', 'vendor/jquery.cookie.js'), 'client');
+	api.add_files(path.join('client', 'vendor/placeholder.js'), 'client');
+	api.add_files(path.join('client', 'foundation.js'), 'client');
 	api.add_files(path.join('client', 'activate-foundation.js'), 'client');
 });
