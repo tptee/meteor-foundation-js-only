b5d370cc69e9557f822da5563ace16da65726436
Tyler Thompson <tyler.thompson@erfmarketing.com>
2013-11-22 11:19:01 -0500

    FastClick fix (hopefully)

--- client/activate-foundation.js
+++ client/activate-foundation.js
@@ -1,5 +1,9 @@
 if (Meteor.isClient) {
   $(document).ready(function () {
+		// Enable FastClick
+		if(typeof FastClick !== 'undefined') {
+		  FastClick.attach(document.body);
+		}
 		$(document).foundation(function (response) {
 			console.log(response.errors);
 		});
