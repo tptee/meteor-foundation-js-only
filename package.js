5948ec5dfae3a18f28b1af1cbe173c93d99734db
Tyler Thompson <tyler.thompson@erfmarketing.com>
2014-02-13 21:48:28 -0500

    Upgraded to 5.1.1

--- package.js
+++ package.js
@@ -6,11 +6,10 @@ Package.on_use(function (api) {
 	api.use('jquery', 'client');
 
 	var path = Npm.require('path');
-	api.add_files(path.join('client', 'vendor/custom.modernizr.js'), 'client');
+	api.add_files(path.join('client', 'vendor/modernizr.js'), 'client');
 	api.add_files(path.join('client', 'vendor/fastclick.js'), 'client');
-	api.add_files(path.join('client', 'vendor/jquery.autocomplete.js'), 'client');
-	api.add_files(path.join('client', 'vendor/jquery.cookie.js'), 'client');
 	api.add_files(path.join('client', 'vendor/placeholder.js'), 'client');
-	api.add_files(path.join('client', 'foundation.js'), 'client');
+	api.add_files(path.join('client', 'vendor/jquery.cookie.js'), 'client');
+	api.add_files(path.join('client', 'foundation.min.js'), 'client');
 	api.add_files(path.join('client', 'activate-foundation.js'), 'client');
 });
